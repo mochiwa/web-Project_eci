42d2792361f4911277e151c6a3a6160dba3eada0
chiappelloni <chiappelloni.nicolas@gmail.com>
2019-12-22 20:51:46 +0100

    remove all php view

--- apache/src/app/identityModule/view/adminPanel.php
+++
@@ -1,9 +0,0 @@
-<section class="block">
-    <h1 class="block__title block__title-center"> Administrator panel </h1>
-    <ul>
-        <li><a href="<?= $router->generateURL('parking.admin') ?>">Parking administration</a></li>
-    </ul>
-</section>
-
-
-    
